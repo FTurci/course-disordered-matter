057bd26ff015ac417622da9cbc284d1ff197afa4
FTurci <turci@me.com>
2025-05-20 17:00:27 +0100

    minimal activity on dlvo

--- quarto/soft-matter/src/plotting.py
+++ quarto/soft-matter/src/plotting.py
@@ -1,12 +1,21 @@
 import matplotlib.pyplot as plt
+import numpy as np
+plt.rcParams['axes.spines.right'] = False
+plt.rcParams['axes.spines.top'] = False
+plt.rcParams['axes.linewidth'] = 0.8
+plt.rcParams['lines.linewidth'] = 1.5
+plt.rcParams['xtick.direction'] = "in"
+plt.rcParams['ytick.direction'] = "in"
+plt.rcParams['legend.frameon'] = False
 import inspect
 
-# def brackify(string):
 
 
 def simple_plot(arr1, arr2):
+    
     # Get caller local variables for name lookup
     fig = plt.figure(figsize=(5, 2))
+
     caller_locals = inspect.currentframe().f_back.f_locals
     # Find names by matching objects
     arr1_name = next((name for name, val in caller_locals.items() if val is arr1), 'arr1')
@@ -23,5 +32,36 @@ def simple_plot(arr1, arr2):
         
     plt.xlabel(rf"${arr1_name}$")
     plt.ylabel(rf"${arr2_name}$")
+    plt.tight_layout()
+    plt.show()
+
+
+
+def multi_plot(*args, xlabel="x", ylabel="y",zero='true'):
+    # args[0] is x data
+    x = args[0]
+
+    # Get caller local variables for name lookup
+    caller_locals = inspect.currentframe().f_back.f_locals
+
+    fig = plt.figure(figsize=(5.1, 3.5))
+
+    # Loop over all y arrays passed after x
+    for a in args[1:]:
+        # Find the first variable name in caller locals whose value is this object
+        arr_name = next((name for name, val in caller_locals.items() if val is a), None)
+        if arr_name is None:
+            arr_name = 'data'
+
+        # Replace underscores with LaTeX subscript
+        if "_" in arr_name:
+            arr_name = arr_name.replace("_", r"_{") + r"}"
+
+        plt.plot(x, a, label=fr"${arr_name}$", alpha=0.7)
+
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.plot(x,np.zeros_like(x),':')
+    plt.legend()
     plt.tight_layout()
     plt.show()
